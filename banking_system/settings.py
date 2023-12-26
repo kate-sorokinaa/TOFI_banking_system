bef5198a912dfe4f6fcb98fc0cb9f09e267218ae
kate-sorokinaa <kate_sorokinaa@mail.ru>
2023-12-26 11:21:57 +0300

    More changes in settings

--- banking_system/settings.py
+++ banking_system/settings.py
@@ -163,7 +163,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
